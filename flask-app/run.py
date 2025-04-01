cefbbae770774979d8421e8f7413871d82fc2c33
xmetkem <xmetkem@stuba.sk>
2025-04-01 21:30:47 +0200

    Spustenie na lokalnej sieti

--- flask-app/run.py
+++ flask-app/run.py
@@ -19,5 +19,5 @@ with app.app_context():
 
 
 if __name__ == '__main__':
-    print("🚀 Starting Flask server on http://127.0.0.1:5000")
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
