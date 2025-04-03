9250fc9524199fa21adb3802221012d83de9f715
xmetkem <xmetkem@stuba.sk>
2025-04-03 11:28:38 +0200

    Zatial fix na iba registraciu patient dokoncitf

--- flask-app/server/routes/register.py
+++ flask-app/server/routes/register.py
@@ -8,21 +8,24 @@ register_service = RegisterService()
 
 logger = logging.getLogger(__name__)
 
+#TODO: Opravit volanie(nech neni /register/patient...)
+
+"""
 @bp.route('/', methods=['GET'], endpoint='register_select')
 def register_select():
-    """Endpoint pre výber typu registrácie."""
+    Endpoint pre výber typu registrácie.
     logger.info("Register select page requested")
     if request.accept_mimetypes['application/json'] >= request.accept_mimetypes['text/html']:
         return jsonify({"msg": "Select registration type"}), 200
     else:
         return render_template('register.html')
-
+"""
 
 @bp.route('/<string:user_type>', methods=['GET'], endpoint='register_form')
 def register_form(user_type):
     """Zobrazenie registračného formulára pre daný user_type."""
     logger.info("Register form requested for user_type: %s", user_type)
-    if user_type not in ['patient', 'doctor', 'technician']:
+    if user_type not in ['patient']: #, 'doctor', 'technician'
         logger.error("Invalid user_type: %s", user_type)
         abort(404)
     # Zobrazíme prázdny formulár:
@@ -33,7 +36,7 @@ def register_form(user_type):
 def register_post(user_type):
     """Spracovanie registračného formulára. Pri chybe posielame `form_data` späť do šablóny."""
     logger.info("Registration POST for user_type: %s", user_type)
-    if user_type not in ['patient', 'doctor', 'technician']:
+    if user_type not in ['patient']: #, 'doctor', 'technician'
         abort(404)
 
     # Vstupná kontrola: zisťujeme, či sú dáta zaslané ako JSON alebo formulárom
